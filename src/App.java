2cb45bb1633ef8b526285d4ead808b0a2d4c9dfd
tenDevs <clsuccess6@gmail.com>
2022-08-12 10:54:46 +0200

    Completed the prime Numbers Program

--- src/App.java
+++ src/App.java
@@ -1,11 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int valA = 60;
-
-        if (valA % 2 == 0) {
-            System.out.println("valA is even");
-        } else {
-            System.out.println("valA is odd");
-        }
+        
     }
 }
