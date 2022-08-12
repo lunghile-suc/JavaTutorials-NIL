4478623ce6ff42f2ff2e7d4eb82c1c99e0441471
tenDevs <clsuccess6@gmail.com>
2022-08-12 11:59:44 +0200

    completed the liner search alogorithm

---
+++ src/LinearSearchAlgo.java
@@ -0,0 +1,19 @@
+public class LinearSearchAlgo {
+    public static void main(String[] args) {
+
+        // the dataset
+        int[] dataNumbers = {29, 98, 76, 56, 45, 20, 67, 34, 65, 78};
+
+        
+        int searchNo = 67;
+
+        for(int i  = 1; i < dataNumbers.length; i++){
+            if(dataNumbers[i] == searchNo){
+                System.out.println(i);
+                break;
+            }else{
+                System.out.println("The item is not present");
+            }
+        }
+    }
+}
